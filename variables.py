b255f45f22622396622b0959d29fc8bea1c89529
Iván Rodríguez <ivanruiz@myuax.com>
2026-02-13 14:21:10 +0100

    tiposVar

---
+++ variables.py
@@ -0,0 +1,14 @@
+vi = 2
+print ("vi:",vi,"la , añade automaticamente un espacio")
+print("type(vi):", type (vi))
+vf = 2.0
+print("vf:", vf)
+print("type (vf):" , type (vf))
+
+vb = True # Ejemplo de comentario: En Paython True/False se escriben con la primera letra en mayusculas
+print("vb:", vb)
+print("type (vb):" , type (vb))
+
+vs = "Hola" 
+print("vs:", vs)
+print("type (vs):" , type (vs))
